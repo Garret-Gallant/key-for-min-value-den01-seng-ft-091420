0c89f833834d53f3ec3cd91401fa30aa7ab3fc58
Garret Gallant <gallanttechnological@gmail.com>
2020-09-03 18:44:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
   
  def key_for_min_value(name_hash)
-  min_value = 1000
+  min_value = 600
   name_value = ""
   name_hash.each do |key, value|
-    if min_value < value
-      value = min_vlue
+    if min_value > value
+      min_value = value
       name_value = key
     end
   end
